47155be9115ae97634faba0c31006eae27ca183b
Michael Weingert <mpweinge@uwaterloo.ca>
2015-08-01 13:43:56 -0700

    Initial checkin

---
+++ cs148project/cs148project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cs148project
+//
+//  Created by Michael Weingert on 2015-08-01.
+//  Copyright (c) 2015 Michael Weingert. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
